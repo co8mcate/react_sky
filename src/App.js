087de1e89ee1f3414440acb73f4006e8637ec216
co8mcate <37420994+co8mcate@users.noreply.github.com>
2020-02-25 13:36:56 -0800

    add a file named Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from React</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const name = "John Doe";
+    const foo = () => "Bar";
+    const loading = false;
+    const showName = true;
+
+    return (
+      <div className="App">
+        <h1>My App</h1>
+        {loading ? (
+          <h4>Loading...</h4>
+        ) : (
+          <h1>Hello from React {showName ? name : null} </h1>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
