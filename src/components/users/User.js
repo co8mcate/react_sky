136fd0401e7618ff3c92308beef5abb98e5ecfeb
co8mcate <37420994+co8mcate@users.noreply.github.com>
2020-05-04 03:52:08 -0700

    decorate user's profile

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -20,6 +20,7 @@ export class User extends Component {
       avatar_url,
       location,
       bio,
+      company,
       blog,
       login,
       html_url,
@@ -53,7 +54,49 @@ export class User extends Component {
               alt=""
               style={{ width: "150px" }}
             />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
           </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-danger">Public Repos: {public_repos}</div>
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
         </div>
       </Fragment>
     );
